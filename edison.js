e9f7d75865748a146c4182eac7152eda0c933cae
alfra <mia.oreskovic@fer.hr>
2016-05-03 01:23:09 +0200

    edison_v2

--- edison.js
+++ edison.js
@@ -1,5 +1,29 @@
 var util = require('util');
 var bleno = require('bleno');
 
+var EdisonService = require('./edison-service');
+
+var name = 'Edison';
+var edisonService = new EdisonService //TO DO nedovrseno
+
 bleno.on('stateChange', function(state) {
-  
+  if (state === 'poweredOn') {
+    bleno.startAdvertising(name, [edisonService.uuid], function(err) {
+      if(err) {
+        console.log(err);
+      }
+    });
+  }
+  else {
+    bleno.stopAdvertising();
+  }
+});
+
+bleno.on('advertisingStart', function(err){
+    if (!err) {
+      conole.log('advertising...');
+      bleno.setServices([
+        edisonService //TO DO nedovrseno
+      ]);
+    }
+});
